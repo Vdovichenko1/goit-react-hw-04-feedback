7433d24b7065038e227333c7cf025589192f5372
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-11-05 13:07:39 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,53 +4,6 @@ import Notification from "./Notification";
 import Section from "./Section";
 import FeedbackOptions from "./FeedbackOptions";
 
-
-// class App1 extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleClickBtn = o => {
-//     this.setState(prevState => {
-//       return {
-//         [o]: prevState[o] + 1,
-//       };
-//     });
-//   };
-
-  // countTotalFeedback = () => {
-  //   return Object.values(this.state).reduce((total, element) => {
-  //     return total + element
-  //   }, 0);
-  // };
-  // countPositiveFeedbackPercentage = () => {
-  //   return ((this.state.good * 100) / this.countTotalFeedback()).toFixed(2);
-  // };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     return (
-//       <div className='div'>
-//         <h1>Cafe Expresso</h1>
-//           <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={{ good, neutral, bad }}
-//             onLeaveFeedback={this.handleClickBtn}
-//           />
-//           </Section>
-//         <Section title="Statistics">
-//         {this.countTotalFeedback() > 0 ? (
-//           <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
-//           ) : <Notification message="There is no feedback"/>}
-//           </Section>
-//       </div>
-//     );
-//   }
-// }
-
-
 export default function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
